824bb9cfe79c49da87f12631fb109143ed95f308
Abijah Immanuel <abijah@Abijah-ZCW-32.local>
2016-10-04 15:03:44 -0400

    final

--- js/script.js
+++ js/script.js
@@ -12,7 +12,20 @@ function PetChat(){
     for(var i=0; i<numPets; i++){
       var type = prompt("What is pet #" + (i+1) + "? (cat, dog, bird)");
       var name = prompt("What is pet #" + (i+1) + "'s name'?");
-      var textOut = type + " " + name + "<br />";
+      //var textOut = type + " " + name + "<br />";
+
+      switch (type) {
+        case "dog": var animal = new Dog(name);
+          break;
+        case "cat": var animal = new Cat(name);
+            break;
+        case "pig": var animal = new pig(name);
+              break;
+        default:
+
+      }
+      var textOut = animal.speak();
+
       display.innerHTML += textOut;
     }
   };
@@ -24,6 +37,34 @@ function PetChat(){
 
     askPetInfo();
   }
+
+  function Pet (name){
+    this.name = name;
+    this.type = "pet";
+    this.sound = "arrrf";
+    this.speak = function(){
+      return "My name is "+ this.name +" i am a "+ this.type + " and I say "+ this.sound + "</br>";
+
+    }
+  }
+  function Dog (name){
+    this.name = name;
+    this.type = "dog";
+    this.sound = "Woof";
+  }
+  function Cat (name){
+    this.name = name;
+    this.type = "cat";
+    this.sound = "Meow";
+  }
+  function Pig (name){
+    this.name = name;
+    this.type = "pig";
+    this.sound = "Oink";
+  }
+  Dog.prototype =new Pet();
+  Cat.prototype = new Pet ();
+  Pig.prototype = new Pet ();
 };
 
 var chat = new PetChat();
